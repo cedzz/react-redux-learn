6bddb928dd0a3b2b42d827efdcde4b82cd6eb722
cedzz <satyamev.porwal@quovantis.com>
2017-07-28 16:32:05 +0530

    Fixed combine reducer simplified version

--- src/routes/Todo/modules/actions.js
+++ src/routes/Todo/modules/actions.js
@@ -34,9 +34,11 @@ export const addTodo = (state, action) => {
 //
 const combineReducers = (reducers) => {
   return (state = {}, action) => {
-    return Object.keys(reducers).reduce((nextState, key) => {
-        nextState[key] = reducers[key](state[key], action)
-    });
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](state[key], action);
+        return nextState;
+      }, {} );
   }
 };
 
